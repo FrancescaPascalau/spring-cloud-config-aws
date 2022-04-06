eb1c7b034b86e724ecec4718d63f3aa3ac53cc06
FrancescaPascalau <pascalaufrancescaa@gmail.com>
2022-04-06 14:23:22 +0200

    Fixed dependencies and aws connection keys.

--- src/main/java/com/francesca/platon/controller/Controller.java
+++ src/main/java/com/francesca/platon/controller/Controller.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 
-    @Value("${parameter-store.sample}")
+    @Value("${parameter-store.value}")
     private String parameter;
 
     @GetMapping
